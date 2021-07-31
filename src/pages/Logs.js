c7a7abdcf949a479dbb48d38f7ec53ba98b4552f
acmendoza11 <acemendoza1113@gmail.com>
2021-08-01 01:47:13 +0800

    Add logs with page and sort

---
+++ src/pages/Logs.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import SortingTable from '../components/logsComponents/SortingTable'
+import LogsTable from '../components/logsComponents/LogsTable'
+
+function QRCodeDisplay() {
+    return (
+        <Container className="extend-height py-5 ">
+                    <Row className="align-items-center justify-content-center">
+                        <Col  md={8} sm={12} className="text-center mx-3 shadow p-3 bg-white">
+                            <p className="card-title">Logs</p><br />
+                            <LogsTable />
+                        </Col>
+                    </Row>
+        </Container>
+          
+    )
+}
+
+
+
+export default QRCodeDisplay
